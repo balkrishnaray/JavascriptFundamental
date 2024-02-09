7d163a868a5aeae8824d9bf1fd23276d028e1939
Balkrishna Ray <balkrishnaroy31@gmail.com>
2024-02-10 01:26:59 +0545

    method

---
+++ arraymethods.js
@@ -0,0 +1,9 @@
+array = [1, 2, 3, 4, 5, 6];
+array.forEach(function (val) {
+  console.log(val);
+});
+
+array1 = ["dog", "cat", "mouse", "lion", "dog", "mouse"];
+console.log(array1.indexOf("cat"));
+console.log(array1.indexOf("mouse", 3));
+console.log(array1.indexOf("giraf", 0));
